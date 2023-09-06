b3ace84aaaeb63243cd84ee4ac2fd8664873cd18
togetherwelove <rkdcksdnr1@gmail.com>
2023-09-06 09:52:02 +0900

    first commit and push

---
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/JeungsangdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.human.jeungsangdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeungsangdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeungsangdiaryApplication.class, args);
+	}
+
+}
---
+++ jeungsangdiary/src/test/java/com/human/jeungsangdiary/JeungsangdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.jeungsangdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeungsangdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
