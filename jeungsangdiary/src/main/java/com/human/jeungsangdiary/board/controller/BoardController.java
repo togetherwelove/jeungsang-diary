6bcfb9f6b5fba0a24415a548d7a7b05da5a1f247
Ssseyoung <ssodrak@naver.com>
2023-09-08 17:26:59 +0900

    예전에 만들었던 게시판으로 우선 올려봐요.. 근데 css와 충돌인지 fragments가 구현이 안되네요 해결 좀 해주세요 ㅎ

---
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/board/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.human.jeungsangdiary.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping(value="/board")
+    public String board() {
+        return "/board";
+    }
+}
+
