d5701836c52731110b46130dc2f0c37879d704ed
Ssseyoung <ssodrak@naver.com>
2023-09-11 11:23:29 +0900

    기본게시판

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/board/controller/BoardController.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/board/controller/BoardController.java
@@ -2,12 +2,31 @@ package com.human.jeungsangdiary.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
-    @GetMapping(value="/board")
-    public String board() {
-        return "/board";
+
+    @GetMapping("/list")
+    public String list() {
+        return "/board_list";
     }
+
+    @GetMapping("/read")
+    public String read() {
+        return "/board_read";
+    }
+
+    @GetMapping("/update")
+    public String update() {
+        return "/board_update";
+    }
+
+     @GetMapping("/insert")
+    public String insert() {
+        return "/board_insert";
+    }
+
 }
 
