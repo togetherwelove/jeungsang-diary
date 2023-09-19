b31df60e36d0841a91463da8ab6525214d4f8296
Ssseyoung <ssodrak@naver.com>
2023-09-19 09:06:48 +0900

    ㅁ

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/JeungsangdiaryApplication.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/JeungsangdiaryApplication.java
@@ -1,11 +1,14 @@
 package com.human.jeungsangdiary;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 import com.human.jeungsangdiary.board.controller.storage.StorageProperties;
+import com.human.jeungsangdiary.board.controller.storage.StorageService;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableConfigurationProperties(StorageProperties.class)
@@ -13,6 +16,14 @@ public class JeungsangdiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JeungsangdiaryApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
