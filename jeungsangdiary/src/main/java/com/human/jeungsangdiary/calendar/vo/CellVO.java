94a68b1a04cddc91f5c001561f362e8273289afc
togetherwelove <rkdcksdnr1@gmail.com>
2023-09-20 18:14:14 +0900

    create file function

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/calendar/vo/CellVO.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/calendar/vo/CellVO.java
@@ -1,5 +1,10 @@
 package com.human.jeungsangdiary.calendar.vo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -8,4 +13,8 @@ public class CellVO {
     private String postDate;
     private String content;
     private String writeDate;
+
+    private List<MultipartFile> files = new ArrayList<>();
+
+    private List<Long> removeFileIds = new ArrayList<>();
 }
