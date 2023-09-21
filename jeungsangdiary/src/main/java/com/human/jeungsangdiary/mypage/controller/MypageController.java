bad7c214acb10b1b5d33ea5fb2869e1d6fdaa2d6
togetherwelove <rkdcksdnr1@gmail.com>
2023-09-21 14:32:03 +0900

    refactoring

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/mypage/controller/MypageController.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/mypage/controller/MypageController.java
@@ -2,12 +2,14 @@ package com.human.jeungsangdiary.mypage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mypage")
 public class MypageController {
     
     @GetMapping(value="/graph")
-    public String showGraphPage() {
+    public String graph() {
         return "mypage/graph";
     }
 }
