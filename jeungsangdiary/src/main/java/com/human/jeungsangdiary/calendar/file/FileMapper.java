a936c29b2c56bae94ce8cdf0d587bc6fa6dd9be6
togetherwelove <rkdcksdnr1@gmail.com>
2023-09-21 12:40:28 +0900

    file download function

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/calendar/file/FileMapper.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/calendar/file/FileMapper.java
@@ -15,15 +15,15 @@ public interface FileMapper {
   List<FileResponse> findAllByCellId(Long cellId);
 
   /**
-   * 파일 리스트 조회
-   * @param ids - PK 리스트
-   * @return 파일 리스트
-   */
-  List<FileResponse> findAllByIds(List<Long> ids);
-
-  /**
    * 파일 삭제
    * @param ids - PK 리스트
    */
   void deleteAllByIds(List<Long> ids);
+
+  /**
+   * 파일 상세정보 조회
+   * @param id - FK
+   * @return 파일 상세정보
+   */
+  FileResponse findById(Long id);
 }
