f2dbb7581251e92f87df8e45dab2f778cd9993a1
DJAlysia <lottie_eve@naver.com>
2023-09-22 17:58:52 +0900

    graph object

--- jeungsangdiary/src/main/java/com/human/jeungsangdiary/mypage/controller/MypageController.java
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/mypage/controller/MypageController.java
@@ -1,13 +1,44 @@
 package com.human.jeungsangdiary.mypage.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.human.jeungsangdiary.mypage.domain.graphObj;
 
 @Controller
 public class MypageController {
     
-    @GetMapping(value="/graph")
-    public String showGraphPage() {
+
+    @GetMapping("/graph")
+    public String graph() {
+
         return "mypage/graph";
     }
+
+	@GetMapping("/restgraph")
+	@ResponseBody
+	public List<graphObj> restgraph(@RequestParam("num") String num) {
+		
+		System.out.println("입력값은: " + num);
+		
+		// 서비스와 디비에서 가져온 샘플 데이터
+		List<graphObj> graphobj = new ArrayList<graphObj>(); // regdate, num 컬럼
+		graphobj.add(new graphObj("내분비", 3));
+		graphobj.add(new graphObj("구강", 15));
+		graphobj.add(new graphObj("피부", 24));
+		graphobj.add(new graphObj("순환기", 35));
+		graphobj.add(new graphObj("소화기", 54));
+		
+        
+		// List<graphObj> test = peoplesv.getListPepleAll();
+		// List<graphObj> test2 = peoplemapper.selectList();
+		
+		return graphobj;
+	}
+
 }
---
+++ jeungsangdiary/src/main/java/com/human/jeungsangdiary/mypage/domain/graphObj.java
@@ -0,0 +1,31 @@
+package com.human.jeungsangdiary.mypage.domain;
+
+public class graphObj {
+
+	private String dttm;
+	private int num;
+	
+	
+	
+	public graphObj(String dttm, int num) {
+		super();
+		this.dttm = dttm;
+		this.num = num;
+	}
+	
+	public String getDttm() {
+		return dttm;
+	}
+	public void setDttm(String dttm) {
+		this.dttm = dttm;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+
+}
